9f932d29c556fa9c7b551a3854de9e356f454006
Your Name <local@localhost>
2026-08-27 20:35:04 -0700

    updated js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ APIs:
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 async function main () {
-  console.log(await(await fetch("https://jsonplaceholder.typicode.com/users")).json());
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersData = await users.json();
+
+  console.log(usersData);
 }
 
 main();

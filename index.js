77627840fac59439dc410cf9b3944694d8b4d01b
Your Name <local@localhost>
2026-08-29 04:09:29 -0700

    updated html

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ async function main () {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
 console.log(
-usersData.map(user => <div class="user-card__container">
-
-<h3>User's Name</h4>
-
-<p><b>Email:</b> email@email.com</p>
-<p><b>Phone:</b> 0000000000</p>
-<p><b>Website:</b> <a href="https://website.website" target="_blank">
-</div>
-</div>
-
-) 
+console.log(
+  usersData.map(user => `
+    <div class="user-card__container">
+      <h3>${user.name}</h3>
+      <p><b>Email:</b> ${user.email}</p>
+      <p><b>Phone:</b> ${user.phone}</p>
+      <p><b>Website:</b> ${user.website}</p>
+    </div>
+  `).join("")
+);
  .join("")
   );
-
 }
 
 main();

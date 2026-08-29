24153f3a7b6d0235b331146a69490453466990be
Your Name <local@localhost>
2026-08-29 04:52:01 -0700

    updated java

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // APIs:
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-async function main () {
+
+async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-console.log(
-console.log(
-  usersData.map(user => `
-    <div class="user-card__container">
-      <h3>${user.name}</h3>
-      <p><b>Email:</b> ${user.email}</p>
-      <p><b>Phone:</b> ${user.phone}</p>
-      <p><b>Website:</b> ${user.website}</p>
-    </div>
-  `).join("")
-);
- .join("")
+
+  console.log(
+    usersData.map(user => `
+      <div class="user-card__container">
+        <h3>${user.name}</h3>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> ${user.website}</p>
+      </div>
+    `).join("")
   );
 }
 
